b50f1d0757a11717dfe14d15cfbb8201691191a3
Florencia Galeano <57403439+florenciagaleano@users.noreply.github.com>
2020-02-10 01:06:24 -0300

    construccion,facturacion y pinturas

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,82 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+	console.log("primer precio "+ precioUno);
+
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+	console.log("segundo precio "+precioDos);
+
+	precioTres=document.getElementById('PrecioTres').value;
+	precioTres=parseInt(precioTres);
+	console.log("tercer precio "+precioTres);
 	
+
+
+	suma=precioUno+precioDos+precioTres;
+	suma=parseInt(suma);
+	console.log("la suma es "+suma);
+
+	alert("La suma es "+suma);
+	
+	//cuando dice mostrar por console o por alert??
+
+
+
 }
 function Promedio () 
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var promedio;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+
+	precioTres=document.getElementById('PrecioTres').value;
+	precioTres=parseInt(precioTres);
+
+
+	promedio=(precioUno+precioDos+precioTres)/3;
+	promedio=parseInt(promedio);
+
+	alert("El promedio es "+promedio);
+
 	
 }
 function PrecioFinal () 
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var precioFinal;
+	var precioFinalIVA;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+
+	precioTres=document.getElementById('PrecioTres').value;
+	precioTres=parseInt(precioTres);
+
+	precioFinal=precioUno+precioDos+precioTres;
+	//es necesario esta var??
+	precioFinalIVA=precioFinal+precioFinal*21/100;
+	
+	alert("El precio con IVA es "+precioFinalIVA);
+
 	
 }
--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -1,13 +1,35 @@
 /*3.	Para el departamento de Pinturas:
-A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
-B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en
+ Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
+ Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
 {
+	var fahrenheit;
+	var centíigrados;
+
+	fahrenheit=document.getElementById('Temperatura').value;
+	fahrenheit=parseInt(fahrenheit);
+
+	centígrados=(fahrenheit-32)/1.8;
+	
+	alert(centígrados+" centígrados son "+fahrenheit+" Fahrenheit");
+
 	
 }
 
 function CentigradosFahrenheit () 
 {
+	var centíigrados;
+	var fahrenheit;
+
+	centígrados=document.getElementById('Temperatura').value;
+	centígrados=parseInt(centígrados);
+
+	fahrenheit=(centígrados*1.8)+32;
+	
+	alert(fahrenheit+" Fahrenheit son "+ centígrados+" grados centígrados");
+
 	
 }
