362765d316b82392ac7b7b5f5451838ff62f13b5
Florencia Galeano <57403439+florenciagaleano@users.noreply.github.com>
2020-02-10 01:07:31 -0300

    ejercicios 1-3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var resultadofinal;
+
+	precio=prompt("Ingrese el precio");
+	precio=parseInt(precio);
+
+	descuento=prompt("Ingrese el porcentaje de descuento","%");
+	descuento=parseInt(descuento);
+
+	resultadofinal=precio-precio*descuento/100;//preguntar si esto está bien hecho
+	document.getElementById('elPrecioFinal').value=resultadofinal;
+
+
 
 }
