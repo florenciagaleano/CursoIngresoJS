f1c7b2f50cc9841fd75fec3589952e6dd9992422
unknown <Florencia Galeano>
2020-02-14 20:05:41 -0300

    No me sale el parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,18 @@
+/*Pedir dos números y mostrar el resultado:
+Si son iguales los muestro concatenados.
+Si el primero es mayor, los resto,
+de lo contrario los sumo.
+Si la suma es mayor a 10 ,además de mostrar el resultado, muestro el mensaje
+"la suma es xxx y supero el 10".*/
+
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=prompt("Ingrese un número");
+	numeroDos=prompt("Ingrese otro número");
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=prompt("Ingree un número");
+	numeroDos=prompt("Ingrese otro número");
+
+	if(numeroUno==numeroDos)
+	{
+		resultado=numeroUno+numeroDos;
+		alert("El resultado es "+resultado);
+		console.log(resultado);
+	}else if (numeroUno<numeroDos)
+	{
+		parseInt(numeroUno);
+		parseInt(numeroDos);
+		resultado=numeroUno+numeroDos;
+		alert("El resultado es "+resultado);
+		console.log(resultado);
+	}else
+	{
+		parseInt(numeroUno);
+		parseInt(numeroDos);
+		resultado=numeroUno-numeroDos;
+		console.log(resultado);
+	}
 
 }
