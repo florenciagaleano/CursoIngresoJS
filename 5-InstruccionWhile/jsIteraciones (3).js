6cea590c2e7646b953d92032d2b47964e6677b5c
unknown <Florencia Galeano>
2020-02-18 23:43:32 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,10 @@ function mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while (clave!="utn750")
+{
+	clave=prompt("CLAVE INCORRECTA","Inténtelo de nuevo");
+}
+
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero;
+	numero=prompt("ingrese un número entre 0 y 10.");
+	numero=parseInt(numero);
+
+	while(numero>10||numero<0)
+	{
+		numero=prompt("Ese número no está entre el 0 y el 9","Escriba un número");
+	}
+
+	document.getElementById('Numero').value=numero;
 
 
 }//FIN DE LA FUNCIÓN
