4cd22efe1c8c113d5b6f5cf2e1271defb9419a39
unknown <Florencia Galeano>
2020-02-19 23:14:40 -0300

    TP estadísticas preguntar lo de lagajo, falta terminarlo

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -1,4 +1,6 @@
-/*RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide realizar una carga de datos validada e ingresada por ventanas emergentes solamente (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
+/*RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide
+ realizar una carga de datos validada e ingresada por ventanas emergentes solamente (para evitar 
+hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
 12.   Los datos requeridos son los siguientes:
 A.	Edad, entre 18 y 90 años inclusive.
 B.	Sexo, “M” para masculino y “F” para femenino
@@ -9,5 +11,42 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	 var edad;
+	 var sexo;
+	 var estadoCivil;
+	 var sueldoBruto;
+	 var legajo;
+	 var nacionalidad;
+
+	 edad=prompt("Ingrese su edad");
+	 edad=parseInt(edad);
+	 sexo=prompt("Ingrese F o M");
+	 estadoCivil=prompt("Ingrese 1, 2, 3 o 4");
+	 sueldoBruto=prompt("Ingrese su sueldo bruto","$");
+	 sueldoBruto=parseInt(sueldoBruto);
+	 
+
+	 while(isNaN(edad)||edad>90||edad<19)
+	 {
+	 	edad=prompt("Edad no válida");
+	 }
+	 while(sexo!="F"&&sexo!="M")
+	 {
+	 	sexo=prompt("Vuelva a intentarlo. Ingrese F o M.");
+	 }
+	 while(isNaN(estadoCivil)||estadoCivil>4||estadoCivil<1)
+	 {
+	 	estadoCivil=prompt("Vuelva a intentarlo. Ingrese 1, 2, 3 o 4.");
+	 }
+	 while (isNaN(sueldoBruto)||sueldoBruto<8000)
+	 {
+	 	sueldoBruto=prompt("Reingrese sueldo");
+	 }
+
+	 document.getElementById('Edad').value=edad;
+	 document.getElementById('Sexo').value=sexo;
+	 document.getElementById('EstadoCivil').value=estadoCivil;
+	 document.getElementById('Sueldo').value=sueldoBruto;
+	 
+
 }
