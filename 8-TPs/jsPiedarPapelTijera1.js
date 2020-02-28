8b56ce7bf649a140fcef4f13bc7c291803ae1899
unknown <Florencia Galeano>
2020-02-28 19:00:27 -0300

    Piedra papel o tijera

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -8,22 +8,77 @@ var eleccionMaquina;
 
 function comenzar()
 {
-	
+	eleccionMaquina=Math.floor(Math.random() * 3)+1;
+	eleccionMaquina=parseInt(eleccionMaquina);
+	console.log(eleccionMaquina);
+
+	switch(eleccionMaquina)
+	{
+		case 1:
+			eleccionMaquina="piedra";
+			break;
+		case 2:
+			eleccionMaquina="papel";
+			break;
+		case 3:
+			eleccionMaquina="tijera";
+			break;
+	}
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	if (eleccionMaquina=="piedra")
+	{
+		alert("Empató");
+	}else
+	{
+		if(eleccionMaquina=="papel")
+		{
+			alert("Perdió");
+		}else
+		{
+			alert("Ganó");
+		}
+	}//hay que hacerlo con switch no con if no?
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	var mensaje;
+	switch(eleccionMaquina)
+	{
+		case "piedra":
+			mensaje="Ganó";
+			break;
+		case "papel":
+			mensaje="Empató";
+			break;
+		case "tijera":
+			mensaje="Perdió";
+			break;
+	}
 
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	var mensaje;
+	switch(eleccionMaquina)
+	{
+		case "piedra":
+			mensaje="Perdió";
+			break;
+		case "papel":
+			mensaje="Ganó";
+			break;
+		case "tijera":
+			mensaje="Empató";
+			break;
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
