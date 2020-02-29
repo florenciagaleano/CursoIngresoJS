c27fa9cc515e6a88c6b73e0b1eceed207d0bd05a
alumno <alumno>
2020-02-29 11:41:29 -0300

    ejercicios parciales clase

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,15 +1,128 @@
 function mostrar()
 {
-	var nombre;
-	var localidad;
+	var contador;
+	var acumuladorCarga;
+	var kilos;
+	var cantidadBolsas;
+	var importeBolsa;
+	var marca;
+	var maximoKilos;
+	var minimoKilos;
+	var importeMenosKilos;
+	var marcaMenosKilos;
+	var marcaMasKilos;
+	var maximoBolsas;
+	var marcaMasBolsas;
+	var maximoImporte;
+	var marcaMasCara;
+	var respuesta;
 
-	nombre=document.getElementById('elNombre').value;
+	contador=0;
 
-	localidad=document.getElementById('laLocalidad').value;
+	do
+	{
+		contador++;
+		do
+		{
+			marca=prompt("Ingrese marca");
+		}while(!isNaN(marca));
 
-	//preguntar si al alert le tengo que poner alguna variable o esta bien si solo pongo alert
-	alert("Usted es "+ nombre+ " y vive en la localidad de "+localidad);
+		do
+		{
+			kilos=prompt("Ingrese kilos por bolsa");
+			kilos=parseInt(kilos);
+		}while(isNaN(kilos)||kilos>1000);
 
+		do
+		{
+			cantidadBolsas=prompt("Ingrese la cantidad de bolsas");
+			cantidadBolsas=parseInt(cantidadBolsas);
+		}while(isNaN(cantidadBolsas)||cantidadBolsas>1000);
 
+		do
+		{
+			importeBolsa=prompt("Ingrese el importe de cada bolsa");
+			importeBolsa=parseInt(importeBolsa);
+		}while(isNaN(importeBolsa)||importeBolsa<0);
+
+		if(contador==1)
+		{
+			maximoKilos=kilos*cantidadBolsas;
+			marcaMasKilos=marca;
+		}else
+		{
+			if(kilos*cantidadBolsas>maximoKilos)
+			{
+				maximoKilos=kilos*cantidadBolsas;
+				marcaMasKilos=marca;
+			}
+		}
+
+		if(contador==1)
+		{
+			maximoBolsas=cantidadBolsas;
+			marcaMasBolsas=marca;
+		}else
+		{
+			if(cantidadBolsas>maximoBolsas)
+			{
+				maximoBolsas=cantidadBolsas;
+				marcaMasBolsas=marca;
+			}
+		}
+
+		if(contador==1)
+		{
+			maximoImporte=importeBolsa;
+			marcaMasCara=marca;
+		}else
+		{
+			if(importeBolsa>maximoImporte)
+			{
+				maximoImporte=importeBolsa;
+				marcaMasCara=marca;
+			}
+		}
+
+		if(contador==1)
+		{
+			minimoKilos=kilos;
+			importeMenosKilos=importeBolsa;
+			marcaMenosKilos=marca;
+		}else
+		{
+			if(minimoKilos>kilos)
+			{
+				minimoKilos=kilos;
+				importeMenosKilos=importeBolsa;
+				marcaMenosKilos=marca;
+
+			}
+		}
+
+		respuesta=prompt("Desea seguir ingresando datos?","si");
+
+	}while(respuesta=="si"||acumuladorCarga<1000);
+
+	console.log("Marca con más kilos en el contenedor: "+marcaMasKilos);
+	console.log("Marca con más bolsas de alimento: "+marcaMasBolsas);
+	console.log("Marca más cara: "+marcaMasCara);
+	console.log("Importre marca menos pesada: "+importeMenosKilos);
+	console.log("Marca que pesa menos: "+marcaMenosKilos);
   
 }
+
+/*
+c)Se pide la carga de datos de un contenedor  de alimento canino con un máximo de carga de 1000kg,
+ cargar hasta que el cliente quiera o se llene el contenedor.
+se pide:
+marca
+kilos por bolsa
+cantidad de bolsa
+importe por bolsa
+
+que marca tiene más kilos en el contenedor
+que marca tiene más bolsas de alimento en el contenedor
+que marca tiene el mayor importe por bolsa de alimento
+el importe y la marca de la bolsa de alimento menos pesada.*/
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,18 +1,135 @@
 
 function mostrar()
 {
-	var ancho;
-	var largo;
-	var perimetro;
+	var contador;
+	var nombre;
+	var cantidadPersonas;
+	var estadia;
+	var formaDePago;
+	var maximoCantidadPersonas;
+	var huespedMasPersonas;
+	var maximoEstancia;
+	var personasMasDias;
+	var pagoMasUsado;
+	var promedioDias;
+	var contadorQR;
+	var contadorTarjeta;
+	var contadorEfectivo;
+	var acumuladorDias;
+	var respuesta;
 
-	ancho=prompt("Ingrese el ancho del rectángulo");
-	largo=prompt("Ingrese el largo del rectángulo");
+	contador=0;
+	contadorEfectivo=0;
+	contadorTarjeta=0;
+	contadorQR=0;
+	acumuladorDias=0;
 
-	perimetro=ancho*2+largo*2;
+	do
+	{
+		contador++;
+		do
+		{
+			nombre=prompt("Ingrese su nombre");
+		}while(!isNaN(nombre));
+	
+		do
+		{
+			cantidadPersonas=prompt("Ingrese la cantidad de personas que se quedarán con usted");
+			cantidadPersonas=parseInt(cantidadPersonas);
+		}while(isNaN(cantidadPersonas)||cantidadPersonas<0);
+	
+		do
+		{
+			estadia=prompt("Ingrese la cantidad de días que se quedará");
+			estadia=parseInt(estadia);
+		}while(isNaN(estadia)||estadia<0);
+	
+		do
+		{
+			formaDePago=prompt("Ingrese forma de pago");
+		}while(!isNaN(formaDePago)||formaDePago!="efectivo"&&formaDePago!="tarjeta"&&formaDePago!="QR");
 
-	alert("El perímetro es "+ perimetro);
-	//preguntar si tengo que poner parseInt y eso
+		if(contador==1)
+		{
+			maximoCantidadPersonas=cantidadPersonas;
+			huespedMasPersonas=nombre;
+		}else
+		{
+			if(cantidadPersonas>maximoCantidadPersonas)
+			{
+				maximoCantidadPersonas=cantidadPersonas;
+				huespedMasPersonas=nombre;
+			}
+		}
+
+		if(contador==1)
+		{
+			maximoEstancia=estadia;
+			personasMasDias=nombre;
+		}else
+		{
+			if(estadia>maximoEstancia)
+			{
+				maximoEstancia=estadia;
+				personasMasDias=nombre;
+			}
+		}
+
+		switch(formaDePago)
+		{
+			case "QR":
+				contadorQR++;
+				break;
+			case "efectivo":
+				contadorEfectivo++;
+				break;				
+			default:
+				contadorTarjeta++;
+				 break;
+		}
+
+		acumuladorDias=acumuladorDias+estadia;
+
+		respuesta=prompt("Desea seguir ingresando huespedes?");
 
 
+	}while(respuesta=="si");
 
+
+	promedioDias=acumuladorDias/contador;
+
+
+	if(contadorQR>contadorEfectivo&&contadorQR>contadorTarjeta)//esto va abajo del while de todo porque sino está mal programado y uso microprocesador de más
+	{
+		pagoMasUsado="QR";
+
+	}else
+	{
+		if(contadorEfectivo>contadorTarjeta)
+		{
+			pagoMasUsado="efectivo";
+
+		}else
+		{
+			pagoMasUsado="tarjeta";
+		}
+	}
+
+	console.log("El huesped que llevó más personas fue: "+huespedMasPersonas);
+	console.log("Persona que se quedó más días: "+personasMasDias);
+	console.log("La forma de pago más usada fue: "+pagoMasUsado);
+	console.log("Promedio de días por persona: "+promedioDias);
+	
 }
+/*
+b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva*/
